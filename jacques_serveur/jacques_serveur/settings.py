c31d7d75cad53b54013d4b304522d339be2f668e
2017attaikis <souhaib.attaiki@student.ecp.fr>
2017-12-03 15:42:29 +0100

    add configuration for db

--- jacques_serveur/jacques_serveur/settings.py
+++ jacques_serveur/jacques_serveur/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'jacques_serveur.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vncgwige',
+        'USER': 'vncgwige',
+        'PASSWORD': 'QJqGs07VIF265xU0I3sHmu41wMPiRpzT',
+        'HOST': 'baasu.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
