b3868834695c4e4b32d5ce9055ad3c075ec9cd50
souhaibattaiki <sohaib.attaiki@gmail.com>
2017-12-08 14:50:36 +0100

    Adding the DB models.

--- jacques_serveur/api/models/bucketlist.py
+++ jacques_serveur/api/models/bucketlist.py
@@ -7,5 +7,40 @@ class Bucketlist(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
+
     def __strt__(self):
         return "{}".format(self.name)
+
+class Degree(models.Model) :
+	name = models.CharField(max_length=255)
+
+class Skills(models.Model) :
+	name = models.CharField(max_length=255)
+
+class Languages(models.Model) :
+	name = models.CharField(max_length=255)
+
+class Contracts(models.Model) :
+	name = models.CharField(max_length=255)
+
+class Locations(models.Model) :
+	name = models.CharField(max_length=255)
+	gpsLatitude = models.BigIntegerField()
+	gpsLongitude = models.BigIntegerField()
+
+class Offer(models.Model):
+	title = models.CharField(max_length=255)
+	degrees = models.CharField(max_length=255)
+	languages = models.CharField(max_length=255)
+	skills = models.CharField(max_length=255)
+	minSalary = models.BigIntegerField()
+	maxSalary = models.BigIntegerField()
+	description = models.CharField(max_length=500)
+	contractType = models.ForeignKey(Contracts, on_delete=models.CASCADE)
+	weeklyWorkTime = models.BigIntegerField()
+	companyName = models.CharField(max_length=255)
+	companyUrl = models.CharField(max_length=255)
+	companyDescription = models.CharField(max_length=255)
+	location = models.ForeignKey(Locations, on_delete=models.CASCADE)
+	experienceName = models.CharField(max_length=255)
+	creationDate = models.DateTimeField(auto_now_add=True)
