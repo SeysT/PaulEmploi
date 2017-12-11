bd06c0ee6eb5c9575d51420a385134aed1f86d9b
Thibaut Seys <thibaut.seys@student.ecp.fr>
2017-12-11 17:15:44 +0100

    add Profile model to handle accepted and refused offers

---
+++ jacques_serveur/api/models/user.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from api.models.offer import Offer
+
+
+class Profile(models.Model):
+    """
+    This class extends the User class from django using a OneToOne relation.
+    + properties:
+        - user: precise to relate to User model from Django
+        - accepted_offers: all offers accepted by user
+        - refused_offers: all offers refused by user
+        - seen_offers: return all offers seen by user (combine accepted and refused offers)
+    + methods:
+        - accept_offer: add the given offer to accepted_offers
+        - refuse_offer: add the given offer to refused_offers
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    accepted_offers = models.ManyToManyField(Offer, related_name='accepted_offers')
+    refused_offers = models.ManyToManyField(Offer, related_name='refused_offers')
+
+    @property
+    def seen_offers(self):
+        """Return all offers seen by the user"""
+        return self.accepted_offers | self.refused_offers
+
+    def accept_offer(self, offer):
+        """This class wrapps the add function off accepted_offers attributes"""
+        self.accepted_offers.add(offer)
+
+    def refuse_offer(self, offer):
+        """This class wrapps the add function off refused_offers attributes"""
+        self.refused_offers.add(offer)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """This function create a Profile every time a User is created"""
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """This function update the Profile model when the User model is saved"""
+    instance.profile.save()
