8a83ecb19d69c5450f0b3faa68ddc17907743cc9
Thibaut Seys <thibaut.seys@student.ecp.fr>
2017-12-15 16:20:52 +0100

    add API routes to accept and refuse offers

--- jacques_serveur/api/urls.py
+++ jacques_serveur/api/urls.py
@@ -1,25 +1,25 @@
-from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
 
-from rest_framework.urlpatterns import format_suffix_patterns
+from api.views.offer import OfferViewSet
 
-from api.views.bucketlist import BucketlistView, BucketlistDetailView
-from api.views.user import UserView, UserDetailView
-from api.views.offer import OfferView, OfferDetailView, OfferExpandView#, DegreeDetailView, SkillDetailView, LanguageDetailView, ContractDetailView, LocationDetailView, CompanyDetailView
 
-urlpatterns = {
-    url(r'^users/$', UserView.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', UserDetailView.as_view()),
-    url(r'^bucketlists/$', BucketlistView.as_view()),
-    url(r'^bucketlists/(?P<pk>[0-9]+)/$', BucketlistDetailView.as_view()),
-    url(r'^offers/$', OfferView.as_view()),
-    url(r'^offers/(?P<pk>[0-9]+)/$', OfferDetailView.as_view()),
-    url(r'^offers/(?P<pk>[0-9]+)/expand/$', OfferExpandView.as_view())#,
-    # url(r'^degree/(?P<pk>[0-9]+)/$', DegreeDetailView.as_view()),
-    # url(r'^skill/(?P<pk>[0-9]+)/$', SkillDetailView.as_view()),
-    # url(r'^language/(?P<pk>[0-9]+)/$', LanguageDetailView.as_view()),
-    # url(r'^contract/(?P<pk>[0-9]+)/$', ContractDetailView.as_view()),
-    # url(r'^location/(?P<pk>[0-9]+)/$', LocationDetailView.as_view()),
-    # url(r'^company/(?P<pk>[0-9]+)/$', CompanyDetailView.as_view()),
-}
+router = DefaultRouter()
+router.register(r'offers', OfferViewSet, base_name='offer')
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = {
+#     url(r'^users/$', UserView.as_view()),
+#     url(r'^users/(?P<pk>[0-9]+)/$', UserDetailView.as_view()),
+#     url(r'^bucketlists/$', BucketlistView.as_view()),
+#     url(r'^bucketlists/(?P<pk>[0-9]+)/$', BucketlistDetailView.as_view()),
+#     url(r'^offers/$', OfferView.as_view()),
+#     url(r'^offers/(?P<pk>[0-9]+)/$', OfferDetailView.as_view()),
+#     url(r'^offers/(?P<pk>[0-9]+)/expand/$', OfferExpandView.as_view()),
+#     # url(r'^degree/(?P<pk>[0-9]+)/$', DegreeDetailView.as_view()),
+#     # url(r'^skill/(?P<pk>[0-9]+)/$', SkillDetailView.as_view()),
+#     # url(r'^language/(?P<pk>[0-9]+)/$', LanguageDetailView.as_view()),
+#     # url(r'^contract/(?P<pk>[0-9]+)/$', ContractDetailView.as_view()),
+#     # url(r'^location/(?P<pk>[0-9]+)/$', LocationDetailView.as_view()),
+#     # url(r'^company/(?P<pk>[0-9]+)/$', CompanyDetailView.as_view()),
+# }
+
+urlpatterns = router.urls
