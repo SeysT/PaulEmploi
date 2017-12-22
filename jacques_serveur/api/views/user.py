dcd4491cf6039af7e3ca61f764bc3abf3587a30e
Thibaut Seys <thibaut.seys@student.ecp.fr>
2017-12-22 14:59:21 +0100

    change profile route to not give user_id to access api

--- jacques_serveur/api/views/user.py
+++ jacques_serveur/api/views/user.py
@@ -1,53 +1,39 @@
-from django.contrib.auth.models import User
-
-from rest_framework import viewsets, status, serializers
-from rest_framework.decorators import detail_route
+from rest_framework import viewsets, status
+from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
 from api.models.user import Profile
-from api.serializers.offer import OfferSerializer, OfferIdSerializer
-from api.serializers.user import UserSerializer, ProfileSerializer
+from api.serializers.offer import OfferSerializer
+from api.serializers.user import ProfileSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+class ProfileViewSet(viewsets.ViewSet):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
 
-    @detail_route(methods=['get'])
-    def profile(self, request, pk=None):
-        if pk is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    def list(self, request):
         try:
-            user = User.objects.get(id=pk)
-            profile = Profile.objects.get(user=user)
-        except User.DoesNotExist or Profile.DoesNotExist:
+            profile = request.user.profile
+        except Profile.DoesNotExist:
             return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = ProfileSerializer(profile)
-        return Response(serializer.data)
+        return Response(self.serializer_class(profile).data)
 
-    @detail_route(methods=['get'])
-    def accepted_offers(self, request, pk=None):
-        if pk is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    @list_route(methods=['get'])
+    def accepted_offers(self, request):
         try:
-            user = User.objects.get(id=pk)
-            profile = Profile.objects.get(user=user)
-            accepted_offers = profile.accepted_offers
-        except User.DoesNotExist or Profile.DoesNotExist:
+            profile = request.user.profile
+        except Profile.DoesNotExist:
             return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
+        accepted_offers = profile.accepted_offers
         serializer = OfferSerializer(accepted_offers, many=True)
         return Response(serializer.data)
 
-    @detail_route(methods=['get'])
-    def offers_to_show(self, request, pk=None):
-        if pk is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    @list_route(methods=['get'])
+    def offers_to_show(self, request):
         try:
-            user = User.objects.get(id=pk)
-            profile = Profile.objects.get(user=user)
-            offers_to_show = profile.offers_to_show
-        except User.DoesNotExist or Profile.DoesNotExist:
+            profile = request.user.profile
+        except Profile.DoesNotExist:
             return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = OfferIdSerializer(offers_to_show, many=True)
+        offers_to_show = profile.offers_to_show
+        serializer = OfferSerializer(offers_to_show, many=True)
         return Response(serializer.data)
-
