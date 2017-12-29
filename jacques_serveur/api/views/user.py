5e57cb69db288c2ca131ae13a5a6e833e4fbe9c3
Manon <manon.leger@ntymail.com>
2017-12-29 23:43:53 +0100

    Implemented the functionality to edit and save user profile

--- jacques_serveur/api/views/user.py
+++ jacques_serveur/api/views/user.py
@@ -1,7 +1,10 @@
+import json
+
 from rest_framework import viewsets, status
-from rest_framework.decorators import list_route
+from rest_framework.decorators import list_route, detail_route
 from rest_framework.response import Response
 
+from api.models.offer import Contract, Location, Interest, Degree, Skill, Language
 from api.models.user import Profile
 from api.serializers.offer import OfferSerializer
 from api.serializers.user import ProfileSerializer
@@ -15,7 +18,42 @@ class ProfileViewSet(viewsets.ViewSet):
         try:
             profile = request.user.profile
         except Profile.DoesNotExist:
-            return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({ 'detail': 'Not found.' }, status=status.HTTP_404_NOT_FOUND)
+        return Response(self.serializer_class(profile).data)
+
+    def put(self, request):
+        try:
+            profile = request.user.profile
+            body = json.loads(request.body.decode())
+        except Profile.DoesNotExist:
+            return Response({ 'detail': 'Not found.' }, status=status.HTTP_404_NOT_FOUND)
+
+        # for item in args.keys():
+        #     try:
+        #         setattr(profile, item, args[item])
+        #     except ValueError or AttributeError:
+        #         pass
+
+        profile.desired_location = Location.objects.get(city_name=body['desired_location'])
+        profile.desired_contract = Contract.objects.get(name=body['desired_contract'])
+
+        for item in body['interests']:
+            item = Interest.objects.get(name=item)
+            profile.interests.add(item)
+        for item in body['degrees']:
+            item = Degree.objects.get(name=item)
+            profile.degrees.add(item)
+        for item in body['skills']:
+            item = Skill.objects.get(name=item)
+            profile.skills.add(item)
+        for item in body['languages']:
+            item = Language.objects.get(name=item)
+            profile.languages.add(item)
+
+        profile.desired_min_salary = body['desired_min_salary']
+        profile.desired_max_salary = body['desired_max_salary']
+
+        profile.save()
         return Response(self.serializer_class(profile).data)
 
     @list_route(methods=['get'])
@@ -37,3 +75,4 @@ class ProfileViewSet(viewsets.ViewSet):
         offers_to_show = profile.offers_to_show
         serializer = OfferIdSerializer(offers_to_show, many=True)
         return Response(serializer.data)
+
--- pierre-client/src/components/Profile.vue
+++ pierre-client/src/components/Profile.vue
@@ -1,26 +1,51 @@
 <template>
   <div>
     <h1>Profile</h1>
-    <p>Location : {{ location }}</p>
-    <p>Interests :</p>
-    <ul>
-      <li v-for="interest in interests">{{ interest }}</li>
-    </ul>
-    <p>Interests :</p>
-    <ul>
-      <li v-for="degree in degrees">{{ degree }}</li>
-    </ul>
-    <p>Skills :</p>
-    <ul>
-      <li v-for="skill in skills">{{ skill }}</li>
-    </ul>
-    <p>Languages :</p>
-    <ul>
-      <li v-for="language in languages">{{ language }}</li>
-    </ul>
-    <p>Minimum salary : {{ min_salary }}</p>
-    <p>Maximum salary : {{ max_salary }}</p>
-    <p>Contract type : {{ contract }}</p>
+    <form>
+      <p>
+        Location :
+        <input v-model="location">
+        <!-- TODO: Ajouter de la validation-->
+      <p>
+        Interests :
+        <select v-model="interests" multiple>
+          <option v-for="interest in available.interests">{{ interest }}</option>
+        </select>
+      </p>
+      <p>
+        Degrees :
+        <select v-model="degrees" multiple>
+          <option v-for="degree in available.degrees">{{ degree }}</option>
+        </select>
+      </p>
+      <p>
+        Skills :
+        <select v-model="skills" multiple>
+          <option v-for="skill in available.skills">{{ skill }}</option>
+        </select>
+      </p>
+      <p>
+        Languages :
+        <select v-model="languages" multiple>
+          <option v-for="language in available.languages">{{ language }}</option>
+        </select>
+      </p>
+      <p>
+        Minimum salary :
+        <input type="number" v-model="min_salary">
+      </p>
+      <p>
+        Maximum salary :
+        <input type="number" v-model="max_salary">
+      </p>
+      <p>
+        Contract type :
+        <select v-model="contract">
+          <option v-for="contract in available.contracts">{{ contract }}</option>
+        </select>
+      </p>
+      <button v-on:click.prevent="save_profile()">Save</button>
+    </form>
   </div>
 </template>
 
@@ -29,6 +54,14 @@
     name: 'profile',
     data () {
       return {
+        // TODO: Charger available en fonction de ce qu'il y a dans la BDD (faire une route)
+        available: {
+          interests: ['informatique', 'électronique', 'mathématiques'],
+          degrees: ['CAP', 'Bac', 'Bac+2', 'Bac+3', 'Bac+4', 'Bac+5'],
+          skills: ['back-end', 'front-end', 'devops', 'data science'],
+          languages: ['Français', 'Anglais'],
+          contracts: ['CDI', 'CDD', 'Stage']
+        },
         location: '',
         interests: [],
         degrees: [],
@@ -53,6 +86,20 @@
           this.max_salary = data.body.desired_max_salary
           this.contract = data.body.desired_contract
         })
+      },
+      save_profile: function () {
+        let url = 'profile/'
+        let body = {
+          desired_location: this.location,
+          desired_contract: this.contract,
+          interests: this.interests,
+          degrees: this.degrees,
+          skills: this.skills,
+          languages: this.languages,
+          desired_min_salary: this.min_salary,
+          desired_max_salary: this.max_salary
+        }
+        this.$http.put(url, body)
       }
     },
     created: function () {
