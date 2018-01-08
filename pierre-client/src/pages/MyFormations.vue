08303a0d3fe5681d48a8e2bbcc34c26d92e29760
Manon <manon.leger@ntymail.com>
2018-01-08 18:09:41 +0100

    Created page 'MyFormations'

---
+++ pierre-client/src/pages/MyFormations.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Card v-for="id in my_ids" :card_id="id" :is_formation=true :key="id"></Card>
+  </div>
+</template>
+
+<script>
+  import Card from '../components/Card.vue'
+
+  export default {
+    name: 'MyFormations',
+    components: {
+      Card
+    },
+    data () {
+      return {
+        my_ids: ['1', '2', '3']
+      }
+    },
+    methods: {
+      get_my_ids: function () {
+        let url = 'profile/kept_formations'
+        this.$http.get(url).then(function (data) {
+          this.my_ids = data.body.map(formation => formation.id.toString())
+        })
+      }
+    },
+    created: function () {
+      this.get_my_ids()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
