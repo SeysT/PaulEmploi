1ce0b88dc37621be5ba634e7f54032390902d08b
Joseph Sayegh <sayegh_j@hotmail.com>
2018-01-11 20:14:48 +0100

    css added

--- pierre-client/src/components/Navbar.vue
+++ pierre-client/src/components/Navbar.vue
@@ -18,8 +18,6 @@
           </div>
         </div>
     </nav>
-    <!-- <router-link to="/">Home</router-link> -->
-    <!-- <router-link to="profile">Profile</router-link> -->
   </div>
 </template>
 
@@ -32,7 +30,7 @@
         default: ''
       }
     }
-  }
+    }
 </script>
 
 <style scoped>
