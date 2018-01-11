b71ac7629da01c3417ace2b33330e4e7fa649923
Thibaut Seys <thibaut.seys@student.ecp.fr>
2018-01-11 17:01:27 +0100

    return HttpResponse object

--- jacques_serveur/jacques_serveur/views/registration.py
+++ jacques_serveur/jacques_serveur/views/registration.py
@@ -1,28 +1,20 @@
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.http import HttpResponse
 from django.utils.datastructures import MultiValueDictKeyError
 from django.views.decorators.http import require_POST
-
-from rest_framework import status
-from rest_framework.response import Response
 
 
 @require_POST
 def register(request):
-   body = request.POST
-   try:
-       email = body['email']
-       password = body['password']
-   except MultiValueDictKeyError:
-       return Response(
-           {'detail': 'You must provide an email and a password.'},
-           status=status.HTTP_400_BAD_REQUEST
-       )
-   try:
-       user = User.objects.create_user(username=email, email=email, password=password)
-   except IntegrityError:
-       return Response(
-           {'detail': 'An account with this email already exists.'},
-           status=status.HTTP_400_BAD_REQUEST
-       )
-   return Response({'result': 'Success'})
+    body = request.POST
+    try:
+        email = body['email']
+        password = body['password']
+    except MultiValueDictKeyError:
+        return HttpResponse({'detail': 'You must provide an email and a password.'}, status=400)
+    try:
+        User.objects.create_user(username=email, email=email, password=password)
+    except IntegrityError:
+        return HttpResponse({'detail': 'An account with this email already exists.'}, status=400)
+    return HttpResponse({'result': 'Success'})
